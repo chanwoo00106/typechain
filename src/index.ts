6ef83ec437d812bd271e7b6842bf2e04966671a6
chanwoo00106 <chan00106@naver.com>
2022-10-11 11:55:04 +0900

    Add block

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-const hello = (): string => 'hi'
+import * as crypto from 'crypto'
 
-console.log(hello())
+interface BlockShape {
+  hash: string
+  prevHash: string
+  height: string
+  data: string
+}
+
+class Block implements BlockShape {
+  public hash: string
+
+  constructor(
+    public prevHash: string,
+    public height: string,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data)
+  }
+
+  static calculateHash(prevHash: string, height: string, data: string): string {
+    const toHash = `${prevHash}${height}${data}`
+    return crypto.createHash('sha256').update(toHash).digest('hex')
+  }
+}
